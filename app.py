b17ba744687dd373c3a530a63635a43560afb036
venuraja79 <venuraja79@gmail.com>
2020-07-09 12:29:15 +0530

    Changed port

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 logging.getLogger("qa-app").setLevel(logging.WARNING)
 
 def get_application() -> FastAPI:
-    application = FastAPI(title="Haystack-API", debug=True, version="0.1")
+    application = FastAPI(title="kaveriai-API", debug=True, version="0.1")
 
     application.add_middleware(
         CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
@@ -33,4 +33,4 @@ Or just try it out directly: curl --request POST --url 'http://127.0.0.1:8000/mo
 )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
